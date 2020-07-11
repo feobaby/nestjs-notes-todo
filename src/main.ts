7c903ab8320b079f3b3d26de938d8f0ae173a41c
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2020-07-11 20:07:10 +0100

    add files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import * as dotenv from "dotenv";
 import { NoteModule } from "../src/modules/note.module";
 
 
